c9311b7f457225821e44c45f01336201028cef61
José Antonio <jocoara1213@gmail.com>
2019-03-02 00:42:05 +0100

    Se crea para incluir las funciones auxiliares que vayan haciendo falta

---
+++ expert-eureka/FuncionesAuxiliares.cpp
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FuncionesAuxiliares.cpp
+ * Author: José Antonio
+ * 
+ * Created on 1 de marzo de 2019, 23:42
+ */
+
+#include "FuncionesAuxiliares.h"
---
+++ expert-eureka/FuncionesAuxiliares.h
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FuncionesAuxiliares.h
+ * Author: José Antonio
+ *
+ * Created on 1 de marzo de 2019, 23:42
+ */
+
+#ifndef FUNCIONESAUXILIARES_H
+#define FUNCIONESAUXILIARES_H
+
+#endif /* FUNCIONESAUXILIARES_H */
